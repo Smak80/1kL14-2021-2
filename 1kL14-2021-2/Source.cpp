d56c6600b1ede86bb6bbd996ac552cd33f64cd40
Sergey Makletsov <smaklets@kpfu.ru>
2021-12-15 09:20:38 +0300

    Сохранение данных о человеке в формате структуры

--- 1kL14-2021-2/Source.cpp
+++ 1kL14-2021-2/Source.cpp
@@ -4,24 +4,6 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Rus");
-	/*
-	//Простой пример работы с бинарными файлами
-
-	ofstream f;
-	f.open("data.txt", ios_base::binary);
-	if (f.is_open()) {
-		int value = 15687696;
-		f.write((const char*)&value, sizeof(value));
-		f.close();
-	}
-	ifstream rf;
-	rf.open("data.txt", ios_base::binary);
-	if (rf.is_open()) {
-		int value;
-		rf.read((char*)&value, sizeof(value));
-		cout << value;
-		rf.close();
-	}*/
 	struct man {
 		char lastname[30];
 		char firstname[30];
