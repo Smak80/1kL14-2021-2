88c1ddabf8d4cd2cbba748ef6e71657e5f4077a1
Sergey Makletsov <smaklets@kpfu.ru>
2021-12-15 09:16:42 +0300

    Сохранение числа в бинарный файл

--- 1kL14-2021-2/Source.cpp
+++ 1kL14-2021-2/Source.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Rus");
-	/*
 	//Простой пример работы с бинарными файлами
 
 	ofstream f;
@@ -21,38 +20,5 @@ int main() {
 		rf.read((char*)&value, sizeof(value));
 		cout << value;
 		rf.close();
-	}*/
-	struct man {
-		char lastname[30];
-		char firstname[30];
-		char birth[11];
-		char address[100];
-		char tel[13];
-		int ege = 250;
-	};
-	man m1 = {
-		"Иванов", 
-		"Иван", 
-		"12.12.2012", 
-		"Казань, Кремлевская, 18", 
-		"+79271110500",
-		243
-	};
-	m1.ege = 234; //обращение к полю структуры по значению
-	man* pm = &m1;
-	pm->ege = 298;//обращение к полю структуры по указателю
-	ofstream wf;
-	wf.open("anketa.dat", ios_base::binary);
-	if (wf.is_open()) {
-		wf.write((const char*)pm, sizeof(man));
-		wf.close();
 	}
-	ifstream rf;
-	man m2;
-	rf.open("anketa.dat", ios_base::binary);
-	if (rf.is_open()) {
-		rf.read((char*)&m2, sizeof(man));
-		rf.close();
-	}
-	cout << m2.address << endl << m2.ege << endl;
 }
